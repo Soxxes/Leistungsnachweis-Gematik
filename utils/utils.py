49c668c6e75722a2107825490c9e17a1222ae445
Soxxes <marc_anmeldungen@proton.me>
2023-06-15 12:23:54 +0200

    added logging decorator

--- utils/utils.py
+++ utils/utils.py
@@ -1,48 +1,19 @@
-import os
-import sys
-import yaml
 import logging
+import sys
 
-import openpyxl
 
-
-def create_folder(name, path=None):
-    try:
-        os.mkdir(name)
-        logging.info(f"Created new folder {name}.")
-    except FileExistsError:
-        logging.info(f"Skipped folder creation of folder '{name}' since it already exists.")
-        pass
-    # change back to working directory
-    if path is not None:
-        os.chdir(path)
-        logging.info(f"Changed directory to: '{path}'.")
-    else:
-        os.chdir(name)
-        logging.info(f"Changed directory to: '{name}'.")
-
-# path should be os pathlike object
-def load_template(path, client, config):
-    try:
-        template = openpyxl.load_workbook(path)
-        skip_style = config.get("Clients").get(client).get("skip_style")
-        if not skip_style:
-            template_sheet = template[config.get("Clients").get(client).get("template_sheet_name")]
-        else:
-            template_sheet = None
-        return template, template_sheet, skip_style
-    except FileNotFoundError:
-        print("[ERROR] No template file found. Please provide a template file for your 'Leistungsnachweis'.")
-        print("Please create a folder 'Template' with a 'template.xlsx' file inside.")
-        input("Aborting. Press any key to exit ...")
-        sys.exit()
-
-def load_config(path):
-    try:
-        with open(path, "r", encoding="utf-8") as ymlfile:
-            config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-        return config
-    except FileNotFoundError:
-        print("[ERROR] Missing config file in template folder.")
-        input("Aborting. Press any key to exit ...")
-        sys.exit()
+# logging decorator
+def add_logging(func):
+    def create_logs(*args, **kwargs):
+        try:
+            res = func(*args, *kwargs)
+            logging.info(f"Successfully ran function {func.__name__} with arguments: {args, kwargs}")
+            return res
+        except Exception as e:
+            logging.info(f"Error in '{func.__name__}' function. Terminated with error:")
+            logging.info(f"Function called with arguments: {args, kwargs}")
+            logging.error(f"{e}")
+            print("[ERROR] Unexpected behavior: Please send the log file to the developers.")
+            input("Press any key to exit ...")
+            sys.exit()
+    return create_logs
